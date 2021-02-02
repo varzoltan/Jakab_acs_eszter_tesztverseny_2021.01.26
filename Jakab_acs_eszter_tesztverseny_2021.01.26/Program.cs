1bf49be5899711a9dfb7d8cf0009b8b2c413ddb6
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-02-02 14:06:55 +0100

    14.06

--- Jakab_acs_eszter_tesztverseny_2021.01.26/Program.cs
+++ Jakab_acs_eszter_tesztverseny_2021.01.26/Program.cs
@@ -38,7 +38,7 @@ namespace Jakab_acs_eszter_tesztverseny_2021._01._26
             //3.feladat
             Console.Write("3. feladat: A versenyző azonosítója = ");
             string kod = Console.ReadLine();
-            string v_valasza;
+            string v_valasza = null;
             for (int i=0; i<n; i++) 
             {
                 if (kod == adatok[i].kod) 
@@ -50,7 +50,36 @@ namespace Jakab_acs_eszter_tesztverseny_2021._01._26
             }
 
             //4.feladat
+            char[] kar_v_valasza = v_valasza.ToCharArray();
+            char[] helyes_tomb = helyes_valaszok.ToCharArray();
+            Console.WriteLine($"4.feladat:\n{helyes_valaszok}\t(a helyes megoldás)");
+            string talalatok = null;
+            for (int i=0; i<kar_v_valasza.Length; i++)
+            {
+                if (kar_v_valasza[i] == helyes_tomb[i])
+                {
+                    talalatok += "+";
+                }
+                else
+                {
+                    talalatok += " ";
+                }
+            }
+            Console.WriteLine($"{talalatok}\t(a versenyző helyes válaszai)");
 
+            //5.feladat
+            Console.Write("5. feladat: A feladat sorszáma= ");
+            int sorszam = int.Parse(Console.ReadLine());
+            int szamlalo = 0;
+            for (int i=0;i<n;i++)
+            {
+                if (helyes_valaszok[sorszam] == adatok[i].valaszok[sorszam])
+                {
+                    szamlalo++;
+                }
+            }
+            double szazalek =  (double)szamlalo  /  n  *  100;
+            Console.WriteLine($"A feladatra {szamlalo} fő, a versenyzők {szazalek.ToString("0.00")}%-a adott helyes választ.");
             Console.ReadKey();
         }
     }
